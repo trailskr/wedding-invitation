db036c593ab8860fbb5641405d660e04df313fc0
Ivan Agafonov <armadil@yandex.ru>
2025-04-12 02:50:30 +0500

    update

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -10,6 +10,6 @@
     stroke-linecap="round"
     stroke-linejoin="round"
   >
-    <path d="M0 22q7 0 12-3 7-4 9-10T19 1q-2-1-5 1T11 6q0 1 .5 1.5t1 0T13 6q0-2-3-4T5 1Q1 3 3 9t9 10q5 3 12 3" />
+    <path d="M1 22q7 0 11-3 7-4 9-10t-2-8q-2-1-5 1t-3 4q0 1 .5 1.5t1 0 .5-1.5q0-2-3-4t-5-1q-4 2-2 8t9 10q5 3 11 3" />
   </svg>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <header class="px-4 py-2 text-center text-14 font-artistic backdrop-blur">
-    Приветствуем
-  </header>
-  <div class="text-center text-8">
-    Как тебя зовут?
+  <div class="mx-auto flex flex-col items-center gap-4 px-10 py-8 container">
+    <Logo class="size-40" />
+    <header class="px-4 py-2 text-14 font-artistic backdrop-blur">
+      Приветствуем
+    </header>
+    <div class="text-8">
+      Как тебя зовут?
+    </div>
   </div>
 </template>
